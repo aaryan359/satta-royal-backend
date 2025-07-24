e6010d9a95057a19c43ef0e30b45f257d0dd7f6e
aaryan359 <aaryanmeena96@gmail.com>
2025-07-24 10:20:58 +0530

    fix email send error

--- src/utils/EmailTransport.ts
+++ src/utils/EmailTransport.ts
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST || 'smtp.gmail.com',
     port: parseInt(process.env.EMAIL_PORT || '587'),
-    secure: process.env.EMAIL_SECURE === 'true', // true for 465, false for other ports
+    secure: process.env.EMAIL_SECURE === 'true', 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
