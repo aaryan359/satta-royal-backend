4f6f72fb8521439e98631e626c2580000d24d3ff
aaryan359 <aaryanmeena96@gmail.com>
2025-07-24 10:13:16 +0530

    added forgot password:

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,42 +3,40 @@ import express, { NextFunction, Request, Response } from 'express';
 import UserController from '../controllers/UserController';
 import { verifyUser } from '../middlewares/authUser';
 import ApiResponse from '../utils/ApiResponse';
-import { token } from 'morgan';
 
 const router = express.Router();
 
-
-//public routes
+// Public routes
 router.post('/register', UserController.register as express.RequestHandler);
 router.post('/login', UserController.login as express.RequestHandler);
 router.post('/google-login', UserController.Oauth as express.RequestHandler);
 
-// router.post('/forgot-password', UserController.forgotPassword as express.RequestHandler);
-// router.post('/reset-password', UserController.resetPassword as express.RequestHandler);
+
+router.post('/forgot-password', verifyUser, (req, res, next) => {
+    Promise.resolve(UserController.forgotPassword(req, res, next)).catch(next);
+});
+router.post('/verify-otp', verifyUser, (req, res, next) => {
+    Promise.resolve(UserController.verifyOtp(req, res, next)).catch(next);
+});
+router.post('/reset-password', verifyUser, (req, res, next) => {
+    Promise.resolve(UserController.resetPassword(req, res, next)).catch(next);
+});
 
 
-// for checking that user is authenticated or not
+
+// Protected routes (require authentication)
 router.get('/check', verifyUser, async (req: Request, res: Response, next: NextFunction) => {
-
-    ApiResponse.success(res,
-        {
-            data: req.user,
-            message: "User is authenticated",
-            statusCode: 201,
-
-        }
-    )
-
-})
-
+    ApiResponse.success(res, {
+        data: req.user,
+        message: "User is authenticated",
+        statusCode: 201,
+    });
+});
 
 router.get('/profile', verifyUser, UserController.getMe as express.RequestHandler);
 router.patch('/update-profile', verifyUser, UserController.updateMe as express.RequestHandler);
 router.delete('/delete-profile', verifyUser, UserController.deleteMe as express.RequestHandler);
 router.patch('/balance', verifyUser, UserController.getBalance as express.RequestHandler);
 router.post('/addbank', verifyUser, UserController.addBank as express.RequestHandler);
-
-
-
 
 export default router;
